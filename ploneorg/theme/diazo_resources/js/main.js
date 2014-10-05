c8389e04045c15e548b498e07c6f1e2c579f8329
Victor Fernandez de Alba <sneridagh@gmail.com>
2014-10-05 19:14:45 +0200

    Update project, use bower to pull dependencies. Use fontello slimmest iconic font. Improve personal bar and other bugs on the theme.

--- ploneorg/theme/diazo_resources/js/main.js
+++ ploneorg/theme/diazo_resources/js/main.js
@@ -4,10 +4,19 @@ $( document ).ready(function() {
   $('.no-csscolumns dt').each(function(){
       $(this).nextUntil('dt').andSelf().wrapAll('<dl class="col-no-css"/>');
   });
-  // $('input').iCheck({
-  //   checkboxClass: 'icheckbox_square-blue',
-  //   radioClass: 'iradio_square-blue',
-  //   // increaseArea: '20%' // optional
-  // });
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_square-blue',
+    radioClass: 'iradio_square-blue',
+    // increaseArea: '20%' // optional
+  });
+  // Disable iCheck on special fields...
+  $('input[type=radio][value=replace]').on('ifChecked', function(event){
+    target_id = $(this).attr('id').replace('replace', 'input')
+    document.getElementById(target_id).disabled=false;
+  });
+  $('input[type=radio][value=replace]').on('ifUnchecked', function(event){
+    target_id = $(this).attr('id').replace('replace', 'input')
+    document.getElementById(target_id).disabled=true;
+  });
 });
 
