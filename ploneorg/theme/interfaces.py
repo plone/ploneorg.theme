12d30d287a8eeed980a1339d4c64e622a46124d9
Coen van der Kamp <c.van.der.kamp@zestsoftware.nl>
2015-11-30 23:24:12 +0100

    Message classes, Add layer, fidle diazo rules.

--- ploneorg/theme/interfaces.py
+++ ploneorg/theme/interfaces.py
@@ -1,6 +1,16 @@
 from zope.interface import Interface
+from plone.theme.interfaces import IDefaultPloneLayer
 
 
 class ICustomTheme(Interface):
     """Marker interface that defines a Zope 3 browser layer.
+    """
+
+
+class IThemeSpecific(IDefaultPloneLayer):
+    """Marker interface that defines a Zope 3 skin layer bound to a Skin
+       Selection in portal_skins.
+       If you need to register a viewlet only for the "YourSkin"
+       skin, this is the interface that must be used for the layer attribute
+       in YourSkin/browser/configure.zcml.
     """
