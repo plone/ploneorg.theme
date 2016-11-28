1b7f577d4276b302d68b857525123a6af6f11d4e
T. Kim Nguyen <tkn@alumni.uwaterloo.ca>
2016-11-27 18:54:51 -0600

    bump version to 1.0, add sponsorship-payments.html page with PayPal and Ammado buttons

--- docs/CHANGES.rst
+++ docs/CHANGES.rst
@@ -7,6 +7,9 @@ Ongoing
 - add sponsorship-payments.html and rule
   [tkimnguyen]
 
+- bump version to 1.0
+  [tkimnguyen]
+
 1.0 (April 2016)
 ----------------
 
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-version = '0.1.dev0'
+version = '1.0'
 
 setup(name='ploneorg.theme',
       version=version,
-      description="Diazo Theme Package for ploneorg",
+      description="Diazo Theme Package for plone.org",
       long_description="",
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -14,9 +14,9 @@ setup(name='ploneorg.theme',
           "Topic :: Software Development :: Libraries :: Python Modules",
           ],
       keywords='',
-      author='Zest Software',
-      author_email='info@zestsoftware.nl',
-      url='http://zestsoftware.nl',
+      author='Plone.org team',
+      author_email='plone.org@plone.org',
+      url='https://plone.org/community/plone.org',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ploneorg'],
